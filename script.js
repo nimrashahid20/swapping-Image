5573aa17ac9eda06ac57de253fdcca9c95aa0a74
Nimra Shahid <nimrashahid02007@gmail.com>
2024-08-21 15:48:43 +0500

    swap

---
+++ script.js
@@ -0,0 +1,17 @@
+function ImageSwap(){
+    document.getElementById("swap").src = "./image/car2.png"
+}
+
+function swapLink(){
+    var linked = document.getElementById("link");
+    linked.href="./image/car2.png"
+}
+
+
+
+
+
+
+
+
+
